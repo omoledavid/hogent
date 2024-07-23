b596af525855c5bd78889a96ec24da0627ecfc84
Your Name <you@example.com>
2024-07-23 16:37:52 +0100

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -95,7 +95,7 @@ class LoginController extends Controller
             } elseif (isset($user) && ($user->role == USER_ROLE_MAINTAINER)) {
                 return redirect()->route('maintainer.dashboard');
             } elseif (isset($user) && ($user->role == USER_ROLE_ADMIN)) {
-                sendEmail($user, 'WELCOME', ['user' => $user->name]);
+                // sendEmail($user, 'WELCOME', ['user' => $user->name]);
                 return redirect()->route('admin.dashboard');
             } else {
                 Auth::logout();
